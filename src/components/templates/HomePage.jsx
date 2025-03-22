006565946e2c81974efd805857c7f48ae0c46678
SanazSobhanloo <cnzsg1@gmail.com>
2025-03-22 17:22:30 +0330

    feat: Create HomePage component as template

---
+++ src/components/templates/HomePage.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import TableCoin from "../modules/TableCoin";
+import { getCoinList } from "../../services/cryptoApi";
+
+function HomePage() {
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch(getCoinList())
+            const json = await res.json()
+            setCoins(json)
+        }
+    }, [])
+    return (
+        <div>
+            <TableCoin coins={coins} />
+        </div>
+    )
+}
+
+export default HomePage
