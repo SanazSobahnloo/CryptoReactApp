71030ca2b41979c20d1c7bbb5657763c4369ddd5
SanazSobhanloo <cnzsg1@gmail.com>
2025-03-24 00:39:53 +0330

    feat: enhance search component styling and layout for better user experience

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { searchCoin } from '../../services/cryptoApi'
 import { RotatingLines } from 'react-loader-spinner'
+import styles from "./Search.module.css"
 
 function Search({ currency, setCurrency }) {
     const [text, setText] = useState('')
@@ -38,14 +39,14 @@ function Search({ currency, setCurrency }) {
         return () => controller.abort()
     }, [text])
     return (
-        <div>
+        <div className={styles.searchBox}>
             <input type="text" placeholder='Search' value={text} onChange={e => setText(e.target.value)} />
             <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
                 <option value="usd">USD</option>
                 <option value="eur">EUR</option>
                 <option value="jpy">JPY</option>
             </select>
-            <div>
+            <div className={styles.searchResult}>
                 {isLoading && <RotatingLines width='50px' height="50px" strokeColor='#4682B4' strokeWidth='2' />}
                 <ul>
                     {coins.map((coin, index) => {
