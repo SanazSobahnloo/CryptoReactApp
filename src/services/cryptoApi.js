fb6f0980473366fd342e61d1d818ebd8b3ef9324
SanazSobhanloo <cnzsg1@gmail.com>
2025-03-23 23:12:50 +0330

    feat: implement search functionality for coins in Search component

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,4 +3,7 @@ const API_KEY = "CG-QhMGkjbEApb7Eg1BBtig6Yt8"
 const getCoinList = (page, currency) => {
     return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=6&page=${page}&x_cg_demo_api_key=${API_KEY}`
 }
-export { getCoinList }
+const searchCoin = (query) => {
+    return `${BASE_URL}/search?x_cg_demo_api_key=${API_KEY}&query=${query}`
+}
+export { getCoinList, searchCoin }
