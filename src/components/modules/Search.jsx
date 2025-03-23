d67f553ad657785ae062e0343e88ea4f3c38ee6b
SanazSobhanloo <cnzsg1@gmail.com>
2025-03-23 23:34:29 +0330

    feat: add loading indicator during coin search and reset loading state on empty input

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { searchCoin } from '../../services/cryptoApi'
+import { RotatingLines } from 'react-loader-spinner'
 
 function Search({ currency, setCurrency }) {
     const [text, setText] = useState('')
     const [coins, setCoins] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         const controller = new AbortController()
 
         setCoins([])
-        if (!text) return
+        if (!text) {
+            setIsLoading(false)
+            return
+        }
         const search = async () => {
             try {
                 const res = await fetch(searchCoin(text), { signal: controller.signal })
                 const json = await res.json()
                 console.log(json)
-                if (json.coins) { setCoins(json.coins) }
+                if (json.coins) {
+                    setIsLoading(false)
+                    setCoins(json.coins)
+                }
                 else {
                     alert(json.status.error_message)
                 }
@@ -25,6 +33,7 @@ function Search({ currency, setCurrency }) {
                 }
             }
         }
+        setIsLoading(true)
         search()
         return () => controller.abort()
     }, [text])
@@ -37,6 +46,7 @@ function Search({ currency, setCurrency }) {
                 <option value="jpy">JPY</option>
             </select>
             <div>
+                {isLoading && <RotatingLines width='50px' height="50px" strokeColor='#4682B4' strokeWidth='2' />}
                 <ul>
                     {coins.map((coin, index) => {
                         return (
